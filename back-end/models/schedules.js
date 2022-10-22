18dea018fae1c1e3e45ea548f62802569d44df40
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-10-22 22:37:44 +0300

    schedule model added

---
+++ back-end/models/schedules.js
@@ -0,0 +1,39 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../utils/databaseConnection');
+
+const Schedule = sequelize.define('schedules',
+{
+    id:
+    {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+
+    sitter_id:
+    {
+        type: DataTypes.UUID,
+    },
+
+    service_id:
+    {
+        type: DataTypes.UUID,
+    },
+
+    date:
+    {
+        type: DataTypes.DATEONLY,
+    },
+
+    start_hour:
+    {
+        type: DataTypes.TIME,
+    },
+
+    end_hour:
+    {
+        type: DataTypes.TIME,
+    }
+})
+
+module.exports = Schedule;
