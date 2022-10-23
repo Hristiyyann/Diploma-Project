2847d7a13bb795b28202634058618ecf9a148973
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-10-23 11:44:09 +0300

    user role model added

---
+++ back-end/models/user_roles.js
@@ -0,0 +1,24 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../utils/databaseConnection');
+
+const UserRole = sequelize.define('user_roles',
+{
+    id:
+    {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+
+    user_id: 
+    {
+        type: DataTypes.UUID,
+    },
+
+    role: 
+    {
+        type: DataTypes.ENUM('Common', 'Sitter', 'Admin'),
+    }
+});
+
+module.exports = UserRole;
