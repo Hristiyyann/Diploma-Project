471740295b411afd683d9e2a9e4ae17dbdbbda29
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-10-23 14:40:46 +0300

    database options updated

--- back-end/utils/databaseConnection.js
+++ back-end/utils/databaseConnection.js
@@ -3,13 +3,22 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(config.database, config.username, config.password,
 {
-    host: config,
+    host: config.host,
     dialect: config.dialect,
     define: 
     {
         freezeTableName: true,
         underscored: true,
+        onDelete: 'NO ACTION',
+        onUpdate: 'CASCADE',
     },
+
+    dialectOptions: 
+    {
+        timezone: "+03:00"
+    },
+
+    timezone: "+03:00",
 });
 
 module.exports = sequelize;
