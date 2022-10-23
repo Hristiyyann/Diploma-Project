b4d373120156147066d490364085f9490c323538
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-10-23 11:05:35 +0300

    sitter model added

---
+++ back-end/models/sitters.js
@@ -0,0 +1,36 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../utils/databaseConnection');
+
+const Sitter = sequelize.define('sitters',
+{
+    id:
+    {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+
+    user_id: 
+    {
+        type: DataTypes.UUID,
+        unique: true,
+    },
+
+    about_sitter:
+    {
+        type: DataTypes.TEXT,
+    },
+
+    city:
+    {
+        type: DataTypes.STRING,
+    },
+
+    
+    neighborhood:
+    {
+        type: DataTypes.STRING,
+    }
+});
+
+module.exports = Sitter;
