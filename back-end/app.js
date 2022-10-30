bbaf3b5fba43816df9d8822a976a517fe3583622
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-10-30 23:51:33 +0200

    models includes simplified

--- back-end/app.js
+++ back-end/app.js
@@ -1,28 +1,19 @@
 const express = require('express');
 const app = express();
 const database = require('./utils/database-connection');
-const bodyParser = require('body-parser');
-const User = require('./models/users.model');
-const Sitter = require('./models/sitters.model');
-const Booking = require('./models/bookings.model');
-const BookingItem = require('./models/booking-items.model');
-const Schedule = require('./models/schedules.model'); 
-const Pets = require('./models/pets.model');
-const SitterCriteria = require('./models/sitter-pet-criteria.model');
-const Review = require('./models/reviews.model');
-const Service = require('./models/services.model');
-const SitterService = require('./models/sitter-services.model');
-const UserNotificationsToken = require('./models/user-notifications-token.model');
-const UserToken = require('./models/user-token.model'); 
+const models = require('./utils/models');
+const authRouter = require('./routes/auth.route');
 
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use('/auth', authRouter);
 
 database.authenticate()
     .then(() => console.log("Successfull connection"))
     .catch(() => console.log("Unsuccessfull connection"))
 
-database.sync({alter: true})
+/* database.sync({alter: true})
     .then(() => console.log("Successfull sync"))
-    .catch((err) => console.log("Unsuccessfull sync", err)) 
+    .catch((err) => console.log("Unsuccessfull sync", err)) */
 
 app.listen(8000);    
---
+++ back-end/utils/models.js
@@ -0,0 +1,18 @@
+const User = require('../models/users.model');
+const Sitter = require('../models/sitters.model');
+const Booking = require('../models/bookings.model');
+const BookingItem = require('../models/booking-items.model');
+const Schedule = require('../models/schedules.model'); 
+const Pet = require('../models/pets.model');
+const SitterCriteria = require('../models/sitter-pet-criteria.model');
+const Review = require('../models/reviews.model');
+const Service = require('../models/services.model');
+const SitterService = require('../models/sitter-services.model');
+const UserNotificationsToken = require('../models/user-notifications-token.model');
+const UserToken = require('../models/user-token.model'); 
+
+module.exports = 
+{
+    User, Sitter, Service, SitterService, UserNotificationsToken, UserToken,
+    Booking, BookingItem, Schedule, Pet, SitterCriteria, Review 
+};
