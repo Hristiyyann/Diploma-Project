8cb706f41b900e857b88f1569f09510bc0529ecd
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-11-03 10:39:50 +0200

    added get roles function
    +update adding tokens to db

--- back-end/utils/helpers.js
+++ back-end/utils/helpers.js
@@ -1,14 +1,18 @@
-const {UserToken} = require('./models');
+const {UserToken, UserRole} = require('./models');
 
-async function addTokenToDB(userId, token) 
+async function addTokensToDB(userId, accessToken, refreshToken) 
 {
     try
     {
-        const addToken = await UserToken.create(
+        await UserToken.bulkCreate([
         {
             user_id: userId,
-            token: token
-        });
+            token: accessToken
+        },
+        {
+            user_id: userId,
+            token: refreshToken
+        }]);
     }
     catch (err)
     {
@@ -16,7 +20,32 @@ async function addTokenToDB(userId, token)
     }
 }
 
+async function getRoles(userId)
+{
+    try
+    {
+        const userRoles = await UserRole.findAll(
+        {
+            where:
+            {
+                user_id: userId
+            },
+            attributes: ['role']
+        });
+    
+        const roles = [];
+        userRoles.forEach(role => { roles.push(role.role); });
+
+        return roles;
+    }
+    catch(error) 
+    {
+        console.log(error);
+    }
+}
+
 module.exports = 
 {
-    addTokenToDB
+    addTokensToDB,
+    getRoles
 }
